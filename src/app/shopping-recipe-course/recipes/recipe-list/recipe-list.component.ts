2018cf1bd532a436ca6b1c18119ac07640a63a53
TonyRahme <tonyrahme96@gmail.com>
2023-04-09 23:54:33 -0400

    feat(recipe): recipe list and detail sections

--- src/app/shopping-recipe-course/recipes/recipe-list/recipe-list.component.ts
+++ src/app/shopping-recipe-course/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a default recipe', 
+    'https://www.cookipedia.co.uk/wiki/images/7/7d/Aperitif_cheese_balls_recipe.jpg'),
+    new Recipe('Another test recipe', 'this is another recipe',
+    'https://live.staticflickr.com/6233/6303474893_ef0cbc1297_b.jpg')
+  ];
 
+  constructor() {}
+
+  ngOnInit() {
+
+  }
 }
