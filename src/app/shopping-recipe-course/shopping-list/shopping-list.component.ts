407bb96b5f350a34a857adfdf77eb79876dd402b
TonyRahme <tonyrahme96@gmail.com>
2023-04-10 01:25:59 -0400

    feat(shopping): shopping list and edit
    added ingredient model
    refactored recipe model

--- src/app/shopping-recipe-course/shopping-list/shopping-list.component.ts
+++ src/app/shopping-recipe-course/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class ShoppingListComponent {
 
+  ingridients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ]
+
+  constructor() {}
+
+  ngOnInit(){
+    
+  }
+  
 }
